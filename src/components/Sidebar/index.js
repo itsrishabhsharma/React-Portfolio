617ccf3d83d81cc154315a1bd24b00623f3ce983
Rishabh Sharma <itsrishabhsharma@gmail.comm>
2023-01-20 07:38:17 +0530

    Added animations

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -6,37 +6,39 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
 
-const Sidebar = () => (
-    <div className='nav-bar'>
-        <Link className='logo' to="/">
-            <img src={LogoS} alt="logo" />
-            <img className="sub-logo" src={LogoSubtitle} alt="slobodan" />
-        </Link>
-        <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
-                <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
-                <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-            </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
-                <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-            </NavLink>
-        </nav>
-        <ul>
-            <li>
-                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/rishabhsharma1234/'>
-                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
-                </a>
-            </li>
-            <li>
-                <a target="_blank" rel='noreferrer' href='https://github.com/itsrishabhsharma'>
-                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
-                </a>
-            </li>
-        </ul>
-    </div>
-)
+const Sidebar = () => {
+    return (
+        <div className='nav-bar'>
+            <Link className='logo' to="/">
+                <img src={LogoS} alt="logo" />
+                <img className="sub-logo" src={LogoSubtitle} alt="Rishabh" />
+            </Link>
+            <nav>
+                <NavLink exact="true" activeclassname="active" to="/">
+                    <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+                    <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+                </NavLink>
+                <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+                    <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+                </NavLink>
+            </nav>
+            <ul>
+                <li>
+                    <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/rishabhsharma1234/'>
+                        <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                    </a>
+                </li>
+                <li>
+                    <a target="_blank" rel='noreferrer' href='https://github.com/itsrishabhsharma'>
+                        <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    )
+};
 
 export default Sidebar
 
